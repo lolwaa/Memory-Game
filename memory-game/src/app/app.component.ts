8610b302f22ab9093bb0b97af9176d7076640cce
lolwaa <lolwaalromi@gmail.com>
2025-04-09 11:07:58 +0300

    home page done

--- memory-game/src/app/app.component.ts
+++ memory-game/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
